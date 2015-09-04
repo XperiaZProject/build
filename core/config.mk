acff198e0ca8e3093ddccd9c38d64eb9412d05d4
Chet Kener <Cl3Kener@gmail.com>
2015-09-04 17:21:03 -0400

    Darkpop all things!
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -619,7 +619,7 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/optipop/sepolicy/sepolicy.mk)
+$(eval include vendor/darkpop/sepolicy/sepolicy.mk)
 
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
